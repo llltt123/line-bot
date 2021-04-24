413ff5277e71987b17afd8f952eaaaa786318d3e
TC <a124567865@gmail.com>
2021-04-24 16:44:22 +0800

    Fix missing import

--- app.py
+++ app.py
@@ -7,7 +7,7 @@ from linebot.exceptions import (
     InvalidSignatureError
 )
 from linebot.models import (
-    MessageEvent, TextMessage, TextSendMessage,
+    MessageEvent, TextMessage, TextSendMessage, StickerSendMessage
 )
 
 app = Flask(__name__)
@@ -45,6 +45,16 @@ def handle_message(event):
     msg = event.message.text
     r = '很抱歉,您說什麼'
 
+    if '給我貼圖' in msg:
+        sticker_message = StickerSendMessage(
+            package_id='1',
+            sticker_id='1'
+        )
+
+        line_bot_api.reply_message(
+            event.reply_token,
+            sticker_message)
+        return # 不return東西的return 為了讓function結束掉
 
     if msg in ['hi', 'Hi']:
         r = '嗨'
